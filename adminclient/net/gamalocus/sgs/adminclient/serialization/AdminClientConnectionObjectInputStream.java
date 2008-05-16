85bfe4340b39ef74d3cf91f58f68d12570aec180
j0rg3n <j0rg3n@43690867-e14b-0410-9436-bf6b3b0914bd>
2008-05-16 09:59:49 +0000

    Fix: SerializationUtils moved back into cotwl2Bundle. Fix: Refactored CustomClassLoaderObjectInputStream into a superclass for ClassLoaderReplacementObjectInputStream.
    Add: NPCCityUpdater now works on the server.
    Add: Now possible to add NPC brains to player estates (requires override switch, though, for safety).
    Add: Admin client can now freely add and remove brains. 
    Fix: NPCEditorInput.revert caused nullpointer exception when there was no npc brain.

---
+++ adminclient/net/gamalocus/sgs/adminclient/serialization/AdminClientConnectionObjectInputStream.java
@@ -0,0 +1,37 @@
+/**
+ * 
+ */
+package net.gamalocus.sgs.adminclient.serialization;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.ObjectInputStream;
+import java.io.ObjectStreamClass;
+import java.util.Map;
+import java.util.logging.Logger;
+
+import sun.reflect.generics.repository.ClassRepository;
+
+import net.gamalocus.sgs.adminclient.connection.AdminClientConnection;
+
+public class AdminClientConnectionObjectInputStream extends CustomClassLoaderObjectInputStream
+{
+	private final static Logger logger = Logger.getLogger(AdminClientConnectionObjectInputStream.class.getName());
+	
+	private AdminClientConnection connection;
+	
+	public AdminClientConnectionObjectInputStream(InputStream in,
+			ClassLoader replacementClassLoader, 
+			Map<String, ObjectStreamClass> classReplacements,
+			AdminClientConnection connection) throws IOException
+	{
+		super(in, replacementClassLoader != null ? replacementClassLoader : 
+					AdminClientConnectionObjectInputStream.class.getClassLoader(), classReplacements);
+		this.connection = connection;
+	}
+
+	public AdminClientConnection getConnection()
+	{
+		return connection;
+	}
+}
