6b1f931c31204537dd4edddb1fad2d08e3f673c6
esko.luontola <esko.luontola@43690867-e14b-0410-9436-bf6b3b0914bd>
2008-06-17 12:16:17 +0000

    ProcessExecutor (originally from project http://plugins.intellij.net/plugin/?id=1605)

---
+++ trunk/darkstar-integration-test/src/main/java/net/orfjackal/darkstar/integration/ProcessExecutorImpl.java
@@ -0,0 +1,83 @@
+/*
+ * Copyright (c) 2007, Esko Luontola. All Rights Reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without modification,
+ * are permitted provided that the following conditions are met:
+ *
+ *     * Redistributions of source code must retain the above copyright notice,
+ *       this list of conditions and the following disclaimer.
+ *     * Redistributions in binary form must reproduce the above copyright notice,
+ *       this list of conditions and the following disclaimer in the documentation
+ *       and/or other materials provided with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+ * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+ * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR
+ * ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+ * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON
+ * ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+ * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+package net.orfjackal.darkstar.integration;
+
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+
+/**
+ * Executes a shell command. Prints the output of the command.
+ *
+ * @author Esko Luontola
+ * @since 1.12.2007
+ */
+public class ProcessExecutorImpl implements ProcessExecutor {
+
+    private final OutputStream stdout;
+    private final OutputStream stderr;
+
+    public ProcessExecutorImpl() {
+        this(System.out, System.err);
+    }
+
+    public ProcessExecutorImpl(OutputStream redirectStdout, OutputStream redirectStderr) {
+        this.stdout = redirectStdout;
+        this.stderr = redirectStderr;
+    }
+
+    public void exec(String command) {
+        try {
+            Process process = Runtime.getRuntime().exec(command);
+            redirect(process.getInputStream(), stdout);
+            redirect(process.getErrorStream(), stderr);
+            process.waitFor();
+
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    private static void redirect(final InputStream from, final OutputStream to) {
+        Thread t = new Thread() {
+            public void run() {
+                byte[] buf = new byte[1024];
+                int len;
+                try {
+                    while ((len = from.read(buf)) > 0) {
+                        to.write(buf, 0, len);
+                    }
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        };
+        t.setDaemon(true);
+        t.start();
+    }
+}
---
+++ trunk/darkstar-integration-test/src/test/java/net/orfjackal/darkstar/integration/ProcessExecutorSpec.java
@@ -0,0 +1,75 @@
+/*
+ * Copyright (c) 2007, Esko Luontola. All Rights Reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without modification,
+ * are permitted provided that the following conditions are met:
+ *
+ *     * Redistributions of source code must retain the above copyright notice,
+ *       this list of conditions and the following disclaimer.
+ *     * Redistributions in binary form must reproduce the above copyright notice,
+ *       this list of conditions and the following disclaimer in the documentation
+ *       and/or other materials provided with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+ * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+ * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR
+ * ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+ * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON
+ * ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+ * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+package net.orfjackal.darkstar.integration;
+
+import jdave.Specification;
+import jdave.junit4.JDaveRunner;
+import org.junit.runner.RunWith;
+
+import java.io.ByteArrayOutputStream;
+import java.io.File;
+
+/**
+ * @author Esko Luontola
+ * @since 6.12.2007
+ */
+@RunWith(JDaveRunner.class)
+public class ProcessExecutorSpec extends Specification<ProcessExecutor> {
+
+    public class AProcessExecutor {
+
+        private ProcessExecutor executor;
+        private ByteArrayOutputStream stdout;
+        private ByteArrayOutputStream stderr;
+
+        public ProcessExecutor create() {
+            stdout = new ByteArrayOutputStream();
+            stderr = new ByteArrayOutputStream();
+            executor = new ProcessExecutorImpl(stdout, stderr);
+            return executor;
+        }
+
+        public void shouldExecuteTheSystemCommand() {
+            File f = new File("testExecuter.tmp");
+            specify(!f.exists());
+            executor.exec("cmd /c mkdir testExecuter.tmp");
+            specify(f.exists());
+            specify(f.isDirectory());
+        }
+
+        public void shouldRedirectStdout() {
+            // "cmd /c" is required by Windows because "echo" is not a file (unlike in Linux) but a shell command
+            executor.exec("cmd /c echo foo");
+            specify(stdout.toString(), should.equal("foo\r\n"));
+            specify(stderr.toString(), should.equal(""));
+        }
+
+        public void shouldRedirectStderr() {
+            executor.exec("cmd /c echo bar>&2");
+            specify(stdout.toString(), should.equal(""));
+            specify(stderr.toString(), should.equal("bar\r\n"));
+        }
+    }
+}
