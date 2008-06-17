6886523fe5b0653b2210da120aab62d134bb41bb
esko.luontola <esko.luontola@43690867-e14b-0410-9436-bf6b3b0914bd>
2008-06-17 12:16:17 +0000

    ProcessExecutor (originally from project http://plugins.intellij.net/plugin/?id=1605)

---
+++ darkstar-integration-test/src/test/java/net/orfjackal/darkstar/integration/DummyProcessExecutor.java
@@ -0,0 +1,51 @@
+/*
+ * Copyright (c) 2007, Esko Luontola. All Rights Reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without modification,
+ * are permitted provided that the following conditions are met:
+ *
+ *     * Redistributions of source code must retain the above copyright notice,
+ *       this list of conditions and the following disclaimer.
+ *     * Redistributions in binary form must reproduce the above copyright notice,
+ *       this list of conditions and the following disclaimer in the documentation
+ *       and/or other materials provided with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+ * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+ * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR
+ * ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+ * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON
+ * ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+ * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+package net.orfjackal.darkstar.integration;
+
+/**
+ * For debugging purposes, does not execute anything - only prints the command.
+ *
+ * @author Esko Luontola
+ * @since 1.12.2007
+ */
+public class DummyProcessExecutor implements ProcessExecutor {
+
+    private static final int LINE_LENGTH = 120;
+
+    public void exec(String command) {
+        System.out.println(DummyProcessExecutor.class.getName() + ".execute(), command:");
+        System.out.print(lineWrap(command));
+    }
+
+    private static String lineWrap(String text) {
+        StringBuilder wrapped = new StringBuilder();
+        for (int begin = 0; begin < text.length(); begin += LINE_LENGTH) {
+            int end = Math.min(begin + LINE_LENGTH, text.length());
+            wrapped.append(text.substring(begin, end));
+            wrapped.append("\n");
+        }
+        return wrapped.toString();
+    }
+}
