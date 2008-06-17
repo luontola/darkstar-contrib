679d8bf7d6a003d9d7e1bab94843b872584e825c
esko.luontola <esko.luontola@43690867-e14b-0410-9436-bf6b3b0914bd>
2008-06-17 17:27:03 +0000

    DarkstarServerRunner: starts up

--- trunk/darkstar-integration-test/src/main/java/net/orfjackal/darkstar/integration/DarkstarServerRunner.java
+++ trunk/darkstar-integration-test/src/main/java/net/orfjackal/darkstar/integration/DarkstarServerRunner.java
@@ -24,6 +24,17 @@
 
 package net.orfjackal.darkstar.integration;
 
+import com.sun.sgs.app.AppListener;
+import com.sun.sgs.impl.kernel.StandardProperties;
+import net.orfjackal.darkstar.integration.util.JavaProcessExecutor;
+import net.orfjackal.darkstar.integration.util.ProcessHolder;
+
+import java.io.ByteArrayOutputStream;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.util.Properties;
+
 /**
  * @author Esko Luontola
  * @since 17.6.2008
@@ -32,4 +43,73 @@ public class DarkstarServerRunner {
 
     public static final String MAIN_CLASS = "com.sun.sgs.impl.kernel.Kernel";
 
+    private final JavaProcessExecutor executor = new JavaProcessExecutor();
+    private final File workingDir;
+
+    private ProcessHolder process;
+
+    public DarkstarServerRunner(File workingDir) {
+        this.workingDir = workingDir;
+    }
+
+    public void start(String appName, Class<? extends AppListener> appListener) {
+        File appRoot = new File(workingDir, "data" + File.separator + appName);
+        File dsdb = new File(appRoot, "dsdb");
+        dsdb.mkdirs();
+        assert dsdb.isDirectory() : "Not a directory: " + dsdb;
+
+        Properties prop = new Properties();
+        prop.setProperty(StandardProperties.APP_NAME, appName);
+        prop.setProperty(StandardProperties.APP_ROOT, appRoot.getAbsolutePath());
+        prop.setProperty(StandardProperties.APP_LISTENER, appListener.getName());
+        prop.setProperty(StandardProperties.APP_PORT, "1139");
+        //prop.setProperty(StandardProperties.MANAGERS, "");
+
+        File propertiesFile = new File(workingDir, appName + ".properties");
+        try {
+            FileOutputStream out = new FileOutputStream(propertiesFile);
+            prop.store(out, null);
+            out.close();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
+        process = executor.exec(mainClass(), propertiesFile.getAbsolutePath());
+    }
+
+    private static Class<?> mainClass() {
+        try {
+            return DarkstarServerRunner.class.getClassLoader().loadClass(MAIN_CLASS);
+        } catch (ClassNotFoundException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public ByteArrayOutputStream getSystemOut() {
+        return process.getSystemOut();
+    }
+
+    public ByteArrayOutputStream getSystemErr() {
+        return process.getSystemErr();
+    }
+
+    public void shutdown() {
+        Process p = process.getProcess();
+        p.destroy();
+        try {
+            p.waitFor();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public boolean isRunning() {
+        Process p = process.getProcess();
+        try {
+            p.exitValue();
+            return false;
+        } catch (IllegalThreadStateException e) {
+            return true;
+        }
+    }
 }
