ff7f60c02781514a22c6f2179fc14107e1292563
esko.luontola <esko.luontola@43690867-e14b-0410-9436-bf6b3b0914bd>
2008-06-17 16:02:05 +0000

    Moved util classes to util package

--- trunk/darkstar-integration-test/src/main/java/net/orfjackal/darkstar/integration/util/ProcessExecutorImpl.java
+++ trunk/darkstar-integration-test/src/main/java/net/orfjackal/darkstar/integration/util/ProcessExecutorImpl.java
@@ -22,25 +22,55 @@
  * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  */
 
-package net.orfjackal.darkstar.integration;
+package net.orfjackal.darkstar.integration.util;
 
+
+import java.io.IOException;
+import java.io.InputStream;
 import java.io.OutputStream;
 
 /**
- * Executes a shell command.
+ * Executes a shell command. Prints the output of the command.
  *
  * @author Esko Luontola
- * @since 30.11.2007
+ * @since 1.12.2007
  */
-public interface ProcessExecutor {
+public class ProcessExecutorImpl implements ProcessExecutor {
 
-    /**
-     * Executes the command and redirects stdout and stderr to System.out and System.err.
-     */
-    int exec(String command);
+    public int exec(String command) {
+        return exec(command, System.out, System.err);
+    }
 
-    /**
-     * Executes the command and redirects stdout and stderr to the specified streams.
-     */
-    int exec(String command, OutputStream stdout, OutputStream stderr);
+    public int exec(String command, OutputStream stdout, OutputStream stderr) {
+        try {
+            Process process = Runtime.getRuntime().exec(command);
+            redirect(process.getInputStream(), stdout);
+            redirect(process.getErrorStream(), stderr);
+            process.waitFor();
+            return process.exitValue();
+
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    private static void redirect(final InputStream from, final OutputStream to) {
+        Thread t = new Thread() {
+            public void run() {
+                byte[] buf = new byte[1024];
+                int len;
+                try {
+                    while ((len = from.read(buf)) > 0) {
+                        to.write(buf, 0, len);
+                    }
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        };
+        t.setDaemon(true);
+        t.start();
+    }
 }
