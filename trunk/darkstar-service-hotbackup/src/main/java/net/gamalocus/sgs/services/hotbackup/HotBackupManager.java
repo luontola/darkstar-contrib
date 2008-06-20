5535a8407f675f868690c152426ced5afe13700c
greisen <greisen@43690867-e14b-0410-9436-bf6b3b0914bd>
2008-06-20 14:07:57 +0000

    - Mavenized

---
+++ trunk/darkstar-service-hotbackup/src/main/java/net/gamalocus/sgs/services/hotbackup/HotBackupManager.java
@@ -0,0 +1,27 @@
+package net.gamalocus.sgs.services.hotbackup;
+
+
+/**
+ * The manager for MySQL lazy synchronization.
+ * 
+ * @author Emanuel Greisen
+ * 
+ */
+public class HotBackupManager
+{
+	/**
+	 * The service backing this manager.
+	 */
+	private HotBackupService service;
+
+	/**
+	 * This is the constructor of the manager, called by SGS.
+	 * 
+	 * @param service
+	 */
+	public HotBackupManager(HotBackupService service)
+	{
+		this.service = service;
+	}
+}
+
