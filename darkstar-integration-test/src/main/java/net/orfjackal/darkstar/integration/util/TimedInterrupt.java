4284d553ad70c582d89e4d3c38d6b7ac94929c62
esko.luontola <esko.luontola@43690867-e14b-0410-9436-bf6b3b0914bd>
2008-07-13 14:42:58 +0000

    Moved TimedInterrupt to darkstar-integration-test

---
+++ darkstar-integration-test/src/main/java/net/orfjackal/darkstar/integration/util/TimedInterrupt.java
@@ -0,0 +1,70 @@
+/*
+ * Copyright (c) 2008, Esko Luontola. All Rights Reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without modification,
+ * are permitted provided that the following conditions are met:
+ *
+ *     * Redistributions of source code must retain the above copyright notice,
+ *       this list of conditions and the following disclaimer.
+ *     * Redistributions in binary form must reproduce the above copyright notice,
+ *       this list of conditions and the following disclaimer in the documentation
+ *       and/or other materials provided with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+ * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+ * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR
+ * ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+ * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON
+ * ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+ * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+package net.orfjackal.darkstar.rpc.util;
+
+/**
+ * Interrupts the specified thread after the timeout has passed, unless the thread
+ * where this interrupter is running is interrupted first.
+ *
+ * @author Esko Luontola
+ * @since 18.6.2008
+ */
+public class TimedInterrupt implements Runnable {
+
+    // TODO: move this utility to a utility project and write tests for it (maybe darkstar-integration-test together with DarkstarIntegrationSpec.RpcTestClient?)
+    // test: wait on java.util.concurrent.BlockingQueue.take, nothing to take -> interrupted
+    // test: wait on java.util.concurrent.BlockingQueue.take, somethign to take -> no interrupt
+
+    private final Thread threadToInterrupt;
+    private final int timeout;
+
+    public static Thread startOnCurrentThread(int timeout) {
+        return start(Thread.currentThread(), timeout);
+    }
+
+    public static Thread start(Thread threadToInterrupt, int timeout) {
+        Thread t = new Thread(new TimedInterrupt(threadToInterrupt, timeout));
+        t.setDaemon(true);
+        t.start();
+        return t;
+    }
+
+    public TimedInterrupt(Thread threadToInterrupt, int timeout) {
+        this.threadToInterrupt = threadToInterrupt;
+        this.timeout = timeout;
+    }
+
+    public void run() {
+        try {
+            assert Thread.currentThread().isDaemon();
+            Thread.sleep(timeout);
+            if (!Thread.currentThread().isInterrupted()) {
+                threadToInterrupt.interrupt();
+            }
+        } catch (InterruptedException e) {
+            // this interrupter was itself interrupted - do nothing
+        }
+    }
+}
