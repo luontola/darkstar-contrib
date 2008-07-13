0e72cc5689a9acd78d0479589ac8eb797f5e4035
esko.luontola <esko.luontola@43690867-e14b-0410-9436-bf6b3b0914bd>
2008-07-13 18:24:45 +0000

    [maven-release-plugin]  copy for tag darkstar-integration-test-1.0

---
+++ tags/darkstar-integration-test-1.0/src/main/java/net/orfjackal/darkstar/integration/DarkstarServer.java
@@ -0,0 +1,220 @@
+/*
+ * Copyright (c) 2008, Esko Luontola. All Rights Reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without modification,
+ * are permitted provided that the following conditions are met:
+ *
+ *     * Redistributions of source code must retain the above copyright notice,
+ *       this list of conditions and the following disclaimer.
+ *     * Redistributions in binary form must reproduce the above copyright notice,
+ *       this list of conditions and the following disclaimer in the documentation
+ *       and/or other materials provided with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+ * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+ * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR
+ * ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+ * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON
+ * ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+ * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+package net.orfjackal.darkstar.integration;
+
+import com.sun.sgs.app.AppListener;
+import com.sun.sgs.impl.kernel.StandardProperties;
+import net.orfjackal.darkstar.integration.util.JavaProcessExecutor;
+import net.orfjackal.darkstar.integration.util.ProcessHolder;
+import net.orfjackal.darkstar.integration.util.StreamWaiter;
+
+import java.io.ByteArrayOutputStream;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.util.Properties;
+import java.util.concurrent.TimeoutException;
+
+/**
+ * @author Esko Luontola
+ * @since 17.6.2008
+ */
+public class DarkstarServer {
+
+    public static final String MAIN_CLASS = "com.sun.sgs.impl.kernel.Kernel";
+    public static final String APP_ROOT = StandardProperties.APP_ROOT;
+    public static final String APP_NAME = StandardProperties.APP_NAME;
+    public static final String APP_LISTENER = StandardProperties.APP_LISTENER;
+    public static final String APP_LISTENER_NONE = StandardProperties.APP_LISTENER_NONE;
+    public static final String APP_PORT = StandardProperties.APP_PORT;
+    public static final String APP_PORT_DEFAULT = "1139";
+
+    public static final String KERNEL_READY_MSG = "Kernel is ready";
+    public static final String APPLICATION_READY_MSG = "application is ready";
+    public static final String APPLESS_CONTEXT_READY_MSG = "non-application context is ready";
+
+    private final JavaProcessExecutor executor = new JavaProcessExecutor();
+    private final File workingDir;
+    private final Properties appProperties;
+    private ProcessHolder process;
+    private StreamWaiter outWaiter;
+    private StreamWaiter errWaiter;
+
+    public DarkstarServer(File workingDir) {
+        this.workingDir = workingDir;
+        appProperties = new Properties();
+        appProperties.setProperty(APP_NAME, "");
+        appProperties.setProperty(APP_LISTENER, APP_LISTENER_NONE);
+        appProperties.setProperty(APP_PORT, APP_PORT_DEFAULT);
+    }
+
+    public String getAppName() {
+        return getProperty(APP_NAME);
+    }
+
+    public void setAppName(String appName) {
+        setProperty(APP_NAME, appName);
+    }
+
+    public Class<? extends AppListener> getAppListener() {
+        String className = getProperty(APP_LISTENER);
+        if (className.equals(APP_LISTENER_NONE)) {
+            return null;
+        }
+        try {
+            return Class.forName(className).asSubclass(AppListener.class);
+        } catch (ClassNotFoundException e) {
+            throw new IllegalArgumentException(e);
+        }
+    }
+
+    public void setAppListener(Class<? extends AppListener> appListener) {
+        setProperty(APP_LISTENER, appListener.getName());
+    }
+
+    public int getPort() {
+        return Integer.valueOf(getProperty(APP_PORT));
+    }
+
+    public void setPort(int port) {
+        setProperty(APP_PORT, Integer.toString(port));
+    }
+
+    public void setProperty(String key, String value) {
+        appProperties.setProperty(key, value);
+    }
+
+    public String getProperty(String key) {
+        return appProperties.getProperty(key);
+    }
+
+    public void start() {
+        start(createAppConfig());
+    }
+
+    public void start(File configFile) {
+        if (isRunning()) {
+            throw new IllegalStateException("Already started");
+        }
+        process = executor.exec(getMainClass(), configFile.getAbsolutePath());
+        outWaiter = new StreamWaiter(getSystemOut());
+        errWaiter = new StreamWaiter(getSystemErr());
+    }
+
+    private File createAppConfig() {
+        if (getAppName().equals("")) {
+            throw new IllegalArgumentException("appName is not set");
+        }
+        File appRoot = createAppRoot(getAppName());
+        appProperties.setProperty(APP_ROOT, appRoot.getAbsolutePath());
+
+        File appConfig = new File(workingDir, getAppName() + ".properties");
+        writeToFile(appConfig, appProperties);
+        return appConfig;
+    }
+
+    private File createAppRoot(String appName) {
+        File appRoot = new File(workingDir, "data" + File.separator + appName);
+        File dataDir = new File(appRoot, "dsdb");
+        dataDir.mkdirs();
+        if (!dataDir.isDirectory()) {
+            throw new RuntimeException("Unable to create data directory: " + dataDir);
+        }
+        return appRoot;
+    }
+
+    private static void writeToFile(File file, Properties properties) {
+        try {
+            FileOutputStream out = new FileOutputStream(file);
+            properties.store(out, null);
+            out.close();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    private static Class<?> getMainClass() {
+        try {
+            return DarkstarServer.class.getClassLoader().loadClass(MAIN_CLASS);
+        } catch (ClassNotFoundException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public void shutdown() {
+        if (process == null) {
+            throw new IllegalStateException("Not started");
+        }
+        Process p = process.getProcess();
+        p.destroy();
+        try {
+            p.waitFor();
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+        outWaiter.dispose();
+        errWaiter.dispose();
+    }
+
+    public boolean isRunning() {
+        try {
+            if (process != null) {
+                process.getProcess().exitValue();
+            }
+            return false;
+        } catch (IllegalThreadStateException e) {
+            // exitValue throws an exception if the process is running
+            return true;
+        }
+    }
+
+    public ByteArrayOutputStream getSystemOut() {
+        return process.getSystemOut();
+    }
+
+    public ByteArrayOutputStream getSystemErr() {
+        return process.getSystemErr();
+    }
+
+    public void waitForKernelReady(int timeout) throws TimeoutException {
+        waitUntilSystemErrContains(KERNEL_READY_MSG, timeout);
+    }
+
+    public void waitForApplicationReady(int timeout) throws TimeoutException {
+        waitUntilSystemErrContains(APPLICATION_READY_MSG, timeout);
+    }
+
+    public void waitForApplessContextReady(int timeout) throws TimeoutException {
+        waitUntilSystemErrContains(APPLESS_CONTEXT_READY_MSG, timeout);
+    }
+
+    public void waitUntilSystemOutContains(String needle, int timeout) throws TimeoutException {
+        outWaiter.waitForBytes(needle.getBytes(), timeout);
+    }
+
+    public void waitUntilSystemErrContains(String needle, int timeout) throws TimeoutException {
+        errWaiter.waitForBytes(needle.getBytes(), timeout);
+    }
+}
---
+++ tags/darkstar-integration-test-1.0/src/test/java/net/orfjackal/darkstar/integration/util/StreamWaiterSpec.java
@@ -0,0 +1,236 @@
+/*
+ * Copyright (c) 2008, Esko Luontola. All Rights Reserved.
+ *
+ * Redistribution and use in source and binary forms, with or without modification,
+ * are permitted provided that the following conditions are met:
+ *
+ *     * Redistributions of source code must retain the above copyright notice,
+ *       this list of conditions and the following disclaimer.
+ *     * Redistributions in binary form must reproduce the above copyright notice,
+ *       this list of conditions and the following disclaimer in the documentation
+ *       and/or other materials provided with the distribution.
+ *
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS" AND
+ * ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+ * WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT OWNER OR CONTRIBUTORS BE LIABLE FOR
+ * ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+ * (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES;
+ * LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON
+ * ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+ * (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE OF THIS
+ * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
+
+package net.orfjackal.darkstar.integration.util;
+
+import jdave.Block;
+import jdave.Specification;
+import jdave.junit4.JDaveRunner;
+import org.junit.runner.RunWith;
+
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.util.concurrent.TimeoutException;
+
+/**
+ * @author Esko Luontola
+ * @since 17.6.2008
+ */
+@RunWith(JDaveRunner.class)
+public class StreamWaiterSpec extends Specification<Object> {
+
+    private static final int DELTA = 50;
+
+    private ByteArrayOutputStream stream;
+    private StreamWaiter waiter;
+
+    public void create() {
+        stream = new ByteArrayOutputStream();
+        waiter = new StreamWaiter(stream);
+    }
+
+    public void destroy() {
+        waiter.dispose();
+    }
+
+
+    public class WhenThereIsNoActivityInTheStream {
+
+        public Object create() {
+            return null;
+        }
+
+        public void theWaiterWillStopWaitingAfterTheTimeout() {
+            long waitTime = waiter.waitForSilenceOf(100);
+            specify(waitTime, should.equal(100, DELTA));
+        }
+    }
+
+    public class WhenThereIsSomeActivityWhileWaiting {
+
+        public Object create() {
+            Thread t = new Thread(new Runnable() {
+                public void run() {
+                    for (int i = 0; i < 10; i++) {
+                        try {
+                            Thread.sleep(10);
+                            stream.write(i);
+                        } catch (InterruptedException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                }
+            });
+            t.start();
+            return null;
+        }
+
+        public void theWaiterWillWaitUntilThereHasBeenNoActivityForTheTimeoutsLength() {
+            long waitTime = waiter.waitForSilenceOf(100);
+            specify(waitTime, should.equal(200, DELTA));
+        }
+    }
+
+    public class WhenThereWasSomeActivityBeforeWaitingStarted {
+
+        public Object create() {
+            stream.write(1);
+            try {
+                Thread.sleep(100);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            return null;
+        }
+
+        public void theWaiterMeasuresTheTimeoutSinceThePastActivity() {
+            long waitTime = waiter.waitForSilenceOf(200);
+            specify(waitTime, should.equal(100, DELTA));
+        }
+    }
+
+    public class WhenTheMonitorIsDisposed {
+
+        public Object create() {
+            stream.write(1);
+            try {
+                Thread.sleep(100);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+            return null;
+        }
+
+        public void theStreamWillNotAnymoreBeMonitoredByADaemonThread() throws InterruptedException {
+            waiter.dispose();
+            Thread.sleep(50);
+            stream.write(1);
+            long waitTime = waiter.waitForSilenceOf(200);
+            specify(waitTime, should.equal(50, DELTA));
+        }
+    }
+
+    public class WhenTheStreamBeingMonitoredIsChanged {
+
+        public Object create() {
+            return null;
+        }
+
+        public void theActivityTimerWillBeReset() throws InterruptedException {
+            Thread.sleep(100);
+            long waitTime1 = waiter.waitForSilenceOf(100);
+            specify(waitTime1, should.equal(0, DELTA));
+
+            waiter.setStream(new ByteArrayOutputStream());
+            long waitTime2 = waiter.waitForSilenceOf(100);
+            specify(waitTime2, should.equal(100, DELTA));
+        }
+
+        public void theNewStreamWillBeMonitored() throws IOException, InterruptedException {
+            stream.write(new byte[10]);
+            Thread.sleep(50);
+
+            final ByteArrayOutputStream newStream = new ByteArrayOutputStream();
+            waiter.setStream(newStream);
+
+            Thread t = new Thread(new Runnable() {
+                public void run() {
+                    for (int i = 0; i < 10; i++) {
+                        try {
+                            Thread.sleep(10);
+                            newStream.write(i);
+                        } catch (InterruptedException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                }
+            });
+            t.start();
+            long waitTime = waiter.waitForSilenceOf(50);
+            specify(waitTime, should.equal(150, DELTA));
+        }
+    }
+
+    public class WhenWaitingForTheOccurrenceOfBytesInTheStream {
+
+        public Object create() {
+            return null;
+        }
+
+        public void nothingIsWrittenToStreamThenWaitingIsTimedOut() {
+            long start = System.currentTimeMillis();
+            specify(new Block() {
+                public void run() throws Throwable {
+                    waiter.waitForBytes("hello".getBytes(), 200);
+                }
+            }, should.raise(TimeoutException.class));
+            long end = System.currentTimeMillis();
+            specify(end - start, should.equal(200, DELTA));
+        }
+
+        public void ifOnlyWrongBytesAppearThenTheWaitingIsTimeOut() {
+            Thread t = new Thread(new Runnable() {
+                public void run() {
+                    for (int i = 0; i < 10; i++) {
+                        try {
+                            Thread.sleep(10);
+                            stream.write("hell".getBytes());
+                        } catch (InterruptedException e) {
+                            e.printStackTrace();
+                        } catch (IOException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                }
+            });
+            t.start();
+            long start = System.currentTimeMillis();
+            specify(new Block() {
+                public void run() throws Throwable {
+                    waiter.waitForBytes("hello".getBytes(), 200);
+                }
+            }, should.raise(TimeoutException.class));
+            long end = System.currentTimeMillis();
+            specify(end - start, should.equal(200, DELTA));
+        }
+
+        public void ifTheRightBytesAppearThenTheWaitingEndsImmediately() throws TimeoutException {
+            Thread t = new Thread(new Runnable() {
+                public void run() {
+                    try {
+                        Thread.sleep(100);
+                        stream.write("Hi, hello.".getBytes());
+                    } catch (InterruptedException e) {
+                        e.printStackTrace();
+                    } catch (IOException e) {
+                        e.printStackTrace();
+                    }
+                }
+            });
+            t.start();
+            long waitTime = waiter.waitForBytes("hello".getBytes(), 200);
+            specify(waitTime, should.equal(100, DELTA));
+        }
+    }
+}
