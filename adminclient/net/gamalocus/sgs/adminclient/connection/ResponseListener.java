716aa1ac845ebbd148e297f68bbaefdc5b7a9f83
greisen <greisen@43690867-e14b-0410-9436-bf6b3b0914bd>
2008-08-30 15:22:00 +0000

    [maven-release-plugin]  copy for tag darkstar-adminclient-1.0.2

--- adminclient/net/gamalocus/sgs/adminclient/connection/ResponseListener.java
+++
@@ -1,8 +0,0 @@
-package net.gamalocus.sgs.adminclient.connection;
-
-import java.io.Serializable;
-
-public interface ResponseListener<RETURN_VALUE extends Serializable> {
-	public void remoteSuccess(final RETURN_VALUE returnValue);
-	public void remoteFailure(final Throwable throwable);
-}
