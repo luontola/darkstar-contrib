e9000bc3a688177a1c00f1677c3ac257c0a8da97
greisen <greisen@43690867-e14b-0410-9436-bf6b3b0914bd>
2008-08-30 15:34:43 +0000

    [maven-release-plugin]  copy for tag darkstar-service-appprofiling-1.0.2

---
+++ tags/darkstar-service-appprofiling-1.0.2/src/main/java/com/gamalocus/sgs/services/profile/AppProfilingManager.java
@@ -0,0 +1,99 @@
+package com.gamalocus.sgs.services.profile;
+
+import java.util.Hashtable;
+import java.util.logging.Logger;
+
+import com.sun.sgs.profile.ProfileConsumer;
+import com.sun.sgs.profile.ProfileOperation;
+import com.sun.sgs.profile.ProfileRegistrar;
+import com.sun.sgs.profile.ProfileCollector.ProfileLevel;
+
+/**
+ * The manager for MySQL lazy synchronization.
+ * 
+ * @author Emanuel Greisen
+ * 
+ */
+public class AppProfilingManager /*implements ProfileProducer*/
+{
+	private final static Logger logger = Logger.getLogger(AppProfilingManager.class.getName());
+	public static boolean is_enabled = false;
+	/**
+	 * The service backing this manager.
+	 */
+	private AppProfilingService service;
+	/**
+	 * An array of operations.
+	 */
+	private static Hashtable<String, ProfileOperation> knownOpNames = new Hashtable<String, ProfileOperation>();
+	private static ProfileConsumer consumer;
+
+	/**
+	 * This is the constructor of the manager, called by SGS.
+	 * 
+	 * @param service
+	 */
+	public AppProfilingManager(AppProfilingService service)
+	{
+		this.service = service;
+	}
+	
+	
+	/**
+	 * Report some operation.
+	 * 
+	 * @param opName
+	 */
+	public static void reportOperation(String opName)
+	{
+		ProfileOperation op = knownOpNames.get(opName);
+		if(op == null)
+		{
+			if (consumer != null)
+			{
+				op = consumer.registerOperation(opName, ProfileLevel.MEDIUM);
+				knownOpNames.put(opName, op);
+			}
+		}
+		if(op != null)
+		{
+			op.report();			
+		}
+		else
+		{
+			//Obviously not using profiling, log to fine
+			logger.fine("reportOperation("+opName+")");
+		}
+	}
+
+
+	public void setProfileRegistrar(ProfileRegistrar profileRegistrar)
+	{
+		consumer =
+            profileRegistrar.registerProfileProducer(this.getClass().getCanonicalName());
+
+		/*
+		if (consumer != null)
+		{
+			for(String opName : service.opNames)
+			{
+				knownOpNames.put(opName, consumer.registerOperation(opName));
+			}
+		}
+		*/
+	}
+
+
+	public static void startReport(String report_identifier)
+	{
+		// TODO: we hope it could be done in a smarter way
+		reportOperation("startReport:"+report_identifier);
+	}
+
+
+	public void endReport()
+	{
+		// Currenlty do nothing.
+	}
+}
+
