d704c34b925961843530723735b58bfdc0f0bf1f
esko.luontola <esko.luontola@43690867-e14b-0410-9436-bf6b3b0914bd>
2008-08-31 10:16:31 +0000

    [maven-release-plugin]  copy for tag darkstar-integration-test-1.0.1

--- darkstar-adminclient/src/main/java/com/gamalocus/sgs/adminclient/connection/ResponseListener.java
+++
@@ -1,8 +0,0 @@
-package com.gamalocus.sgs.adminclient.connection;
-
-import java.io.Serializable;
-
-public interface ResponseListener<RETURN_VALUE extends Serializable> {
-	public void remoteSuccess(final RETURN_VALUE returnValue);
-	public void remoteFailure(final Throwable throwable);
-}
